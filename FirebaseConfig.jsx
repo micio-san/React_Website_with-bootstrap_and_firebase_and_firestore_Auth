2708d5ac9201bced09a2fad8d5fa9a353aaba04f
Mist Uber <danyrooke@gmail.com>
2022-12-24 08:47:09 +0100

    Removed Config
    Removed Firebase Config

--- FirebaseConfig.jsx
+++ FirebaseConfig.jsx
@@ -4,12 +4,6 @@ import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
 
-    authDomain: "i-have-no-idea-what-i-m-f1607.firebaseapp.com",
-    projectId: "i-have-no-idea-what-i-m-f1607",
-    storageBucket: "i-have-no-idea-what-i-m-f1607.appspot.com",
-    messagingSenderId: "972410165015",
-    appId: "1:972410165015:web:e86311658ee742eeda4952",
-    measurementId: "G-2742PKX67T"
 };
 
 // Initialize Firebase
